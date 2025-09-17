e679e7526c12328bc8d61de85cd45797177e1ebb
Vanchika777 <vanchika.raghav7@gmail.com>
2025-09-17 15:56:43 +0530

    Added my Image slideshow app

---
+++ Image slideshow app.py
@@ -0,0 +1,43 @@
+from itertools import cycle
+from PIL import Image, ImageTk
+import time
+import tkinter as tk
+
+root=tk.Tk()
+root.title("Image Slideshow Viewer")
+
+#List of Image Path
+image_paths =[
+    r"C:\Users\Dell\Documents\wallpaper\wallpaper5.jpg",
+    r"C:\Users\Dell\Documents\wallpaper\wallpaper2.jpg",
+    r"C:\Users\Dell\Documents\wallpaper\wallpaper1.jpg",
+    r"C:\Users\Dell\Documents\wallpaper\wallpaper6.jpg",
+    r"C:\Users\Dell\Documents\wallpaper\wallpaper totoro.jpg"
+]
+
+#Resize the images to 1080x1080
+image_size=(600,700)
+images=[Image.open(path). resize(image_size) for path in image_paths]
+photo_images=[ImageTk.PhotoImage(image) for image in images]
+
+label = tk.Label(root)
+label.pack()
+
+def update_image():
+    for photo_image in photo_images:
+        label.config(image=photo_image)
+        label.update()
+        time.sleep(3)
+
+slideshow = cycle(photo_images)
+
+def start_slideshow():
+    for _ in range(len(image_paths)):
+        update_image()
+
+play_button = tk.Button(root, text='Play Slideshow', command=start_slideshow)
+play_button.pack()
+
+root.mainloop()
+
+
